b519928f2a34ad08fee4a5576b873c7ba03d45b5
javeria-farooq <gettingeducation6@gmail.com>
2024-01-15 20:13:02 +0500

    5th JS Assignment

---
+++ app.js
@@ -0,0 +1,27 @@
+// let firstName = 'Jav '
+// let lastName = 'Farooq'
+
+// alert(firstName + lastName)
+
+// let firstName = 'Jav'
+// let lastName = 'Farooq'
+
+// alert(firstName , lastName)
+
+// ab mene  kaha h k num ki value h 5 or res k variable mn num k increment ko store krwa rh hun
+
+// let num = 5
+// let res = num++
+// // document.write(res + num)
+// document.write(num)
+
+// t abhi jb mn document.write krke res ki value dkhngi t wo 5 hi btaega q k ye post increment h or ismn ek step baad
+// increment hta h.ek step baad ka mtlb h k jb mn res ki value yni num++ ki value mlum krngi uske baad jb num ki value
+// mlum krngi t wo ek ka increment krke dikhaega isi trhn agr mn jb num++ ki value yni res ki vallue print krwaarhi hn
+// oosi wqt agr mn plus k sth num ki value bh print krwna chahun t kia hga num++ ki value th 5 phr usmn plus krwai num
+// ki value jo ab increment hoke 6 hgai h to num++ ki value 5 or numm ki value 6 dnon ko agr ek sth print krwaoungi t
+// zhir h mn is trhn lkhungi
+
+// document.write(res + num)
+
+// ab q k res mn num++ ki value h t islye num++ ki value mn num ki value jo ab 6 hgai h wo dnon plus hoke ans aayega 11.
